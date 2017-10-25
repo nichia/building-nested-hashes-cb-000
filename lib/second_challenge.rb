b0c4612b1b4f3146e8f8fdf3640a88b5a259248f
Ni Chia <nichia@gmail.com>
2017-10-25 18:38:32 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -10,7 +10,7 @@ def second_challenge
     :capulet => {
       #your key/value pairs here!
       :patriarch => {},
-      :matriarch => {}, 
+      :matriarch => {},
       :heroine => {},
       :heroine_friends => []
       }}
