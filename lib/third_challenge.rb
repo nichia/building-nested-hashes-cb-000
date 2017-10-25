a4fe93a6e2240c7091de87b885ed6800ccedde1d
Ni Chia <nichia@gmail.com>
2017-10-25 18:49:20 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,7 +3,7 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
-        name: "Load Montague",
+        name: "Lord Montague",
         age: "53"
         },
       :matriarch => {
