a230d565126666f518d23bf18046e4bd2acb9215
Ni Chia <nichia@gmail.com>
2017-10-25 18:18:36 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,7 +9,7 @@ def fourth_challenge
         {name: "Bevolio", age: "17", attitude: "worried"}
         {name: "Mercutio", age: "18", attitude: "hot_headed"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -22,6 +22,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
